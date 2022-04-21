309a3f8ebe12b846b7d7cf06d0fd675313c36d68
Dividesbyzer0 <54127744+zoomdbz@users.noreply.github.com>
2022-04-21 18:08:33 -0400

    Create url2cidr.sh

---
+++ url2cidr.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for i in `cat ${1+"$@"} 2>/dev/null || echo ${1+"$@"}`; do nslookup "$i" | grep "Address: " | tr -s '[[:blank:]]' | cut -d ":" -s -f 2-11 | sort -u | sed -e 's/^[ \t]*//' | while IFS= read -r line; do whois -IH "$line" | grep 'OrgName: \|CIDR: ' | tr -s '[[:blank:]]' | cut -d ":" -f 2 | sort -u | awk '$1=$1'; done;done
